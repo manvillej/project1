c36e312b653b4a4de8d9ba3dc6f261e2db8feb7d
manvillej <jeffmanvillejr@gmail.com>
2018-07-08 12:57:30 -0400

    Added _nav.html
    next fixing links to other pages

--- app/routes.py
+++ app/routes.py
@@ -1,37 +1,39 @@
 from flask import render_template
 from app import app
 
+brand='Manville'
+
 @app.route("/")
 def index():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='Home', user=user)
+    return render_template('base.html', title='Home', brand=brand, user=user)
 
 @app.route('/login')
 def login():
     user = {'username':'Jeff'}
-    return render_template('index.html', title='login', user=user)
+    return render_template('base.html', title='login', brand=brand, user=user)
 
 @app.route('/logout')
 def logout():
     user = {'username':'Jeff'}
-    return render_template('index.html', title='logout', user=user)
+    return render_template('base.html', title='logout', brand=brand, user=user)
 
 @app.route('/register')
 def register():
     user = {'username':'Jeff'}
-    return render_template('index.html', title='register', user=user)
+    return render_template('base.html', title='register', brand=brand, user=user)
 
 @app.route('/search')
 def search():
     user = {'username':'Jeff'}
-    return render_template('index.html', title='search', user=user)
+    return render_template('base.html', title='search', brand=brand, user=user)
 
 @app.route('/location/<int:zipcode>')
 def location(zipcode):
     user = {'username':'Jeff'}
-    return render_template('index.html', title='location', user=user)
+    return render_template('base.html', title='location', brand=brand, user=user)
 
 @app.route('/api/<int:zipcode>')
 def api(zipcode):
     user = {'username':'Jeff'}
-    return render_template('index.html', title='api', user=user)
+    return render_template('base.html', title='api', brand=brand, user=user)
