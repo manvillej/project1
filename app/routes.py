b683c1694a8829d5fcb75507490d3b88be6fc1a9
manvillej <jeffmanvillejr@gmail.com>
2018-07-08 13:45:50 -0400

    Added SiteConfig NamedTuple for sitewide variables
    Added SiteConfig Named variable for sitewide variables like brand and external urls
    
    its really awesome

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,49 @@
 from flask import render_template
 from app import app
+from collections import namedtuple
 
-brand='Manville'
-project_url='https://github.com/manvillej/project1'
+project_variables = ['brand', 'project_url', 'github_url', 'social_url']
+SiteConfig = namedtuple('SiteConfig', " ".join(project_variables))
+
+site_config = SiteConfig(
+    brand='Manville',
+    project_url='https://github.com/manvillej/project1',
+    github_url='https://github.com/manvillej',
+    social_url='https://twitter.com/JeffManville',
+    )
+
 
 @app.route("/")
 def index():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='Home', brand=brand, project_url=project_url, user=user)
+    return render_template('base.html', title='Home', config=site_config, user=user)
 
 @app.route('/login')
 def login():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='login', brand=brand, project_url=project_url, user=user)
+    return render_template('base.html', title='login', config=site_config, user=user)
 
 @app.route('/logout')
 def logout():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='logout', brand=brand, project_url=project_url, user=user)
+    return render_template('base.html', title='logout', config=site_config, user=user)
 
 @app.route('/register')
 def register():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='register', brand=brand, project_url=project_url, user=user)
+    return render_template('base.html', title='register', config=site_config, user=user)
 
 @app.route('/search')
 def search():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='search', brand=brand, project_url=project_url, user=user)
+    return render_template('base.html', title='search', config=site_config, user=user)
 
 @app.route('/location/<int:zipcode>')
 def location(zipcode):
     user = {'username':'Jeff'}
-    return render_template('base.html', title='location', brand=brand, project_url=project_url, user=user)
+    return render_template('base.html', title='location', config=site_config, user=user)
 
 @app.route('/api/<int:zipcode>')
 def api(zipcode):
     user = {'username':'Jeff'}
-    return render_template('base.html', title='api', brand=brand, project_url=project_url, user=user)
+    return render_template('base.html', title='api', config=site_config, user=user)
