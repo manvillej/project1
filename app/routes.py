276b41f7a1cad85e559bc16a1b54b420a3806259
manvillej <jeffmanvillejr@gmail.com>
2018-07-08 13:12:53 -0400

    Added footer template & header updated
    added footer template
    changed _nav.html to _header.html to resemble _footer.html naming

--- app/routes.py
+++ app/routes.py
@@ -2,38 +2,39 @@ from flask import render_template
 from app import app
 
 brand='Manville'
+project_url='https://github.com/manvillej/project1'
 
 @app.route("/")
 def index():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='Home', brand=brand, user=user)
+    return render_template('base.html', title='Home', brand=brand, project_url=project_url, user=user)
 
 @app.route('/login')
 def login():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='login', brand=brand, user=user)
+    return render_template('base.html', title='login', brand=brand, project_url=project_url, user=user)
 
 @app.route('/logout')
 def logout():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='logout', brand=brand, user=user)
+    return render_template('base.html', title='logout', brand=brand, project_url=project_url, user=user)
 
 @app.route('/register')
 def register():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='register', brand=brand, user=user)
+    return render_template('base.html', title='register', brand=brand, project_url=project_url, user=user)
 
 @app.route('/search')
 def search():
     user = {'username':'Jeff'}
-    return render_template('base.html', title='search', brand=brand, user=user)
+    return render_template('base.html', title='search', brand=brand, project_url=project_url, user=user)
 
 @app.route('/location/<int:zipcode>')
 def location(zipcode):
     user = {'username':'Jeff'}
-    return render_template('base.html', title='location', brand=brand, user=user)
+    return render_template('base.html', title='location', brand=brand, project_url=project_url, user=user)
 
 @app.route('/api/<int:zipcode>')
 def api(zipcode):
     user = {'username':'Jeff'}
-    return render_template('base.html', title='api', brand=brand, user=user)
+    return render_template('base.html', title='api', brand=brand, project_url=project_url, user=user)
