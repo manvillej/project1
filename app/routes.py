ae8423c90d6e22514b52c5bf0c710b7e7b0bc97d
manvillej <jeffmanvillejr@gmail.com>
2018-07-08 00:52:46 -0400

    Added basic templating

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    user = {'username':'Jeff'}
+    return render_template('index.html', title='Home', user=user)
--- config.py
+++ config.py
@@ -6,7 +6,12 @@ class Config(object):
 	    raise RuntimeError("DATABASE_URL is not set")
 	SESSION_PERMANENT = False
 	SESSION_TYPE = "filesystem"
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or '8vd%4la%7TuP5Od3vG6DDaxTuU6X%U%aGRbiS6Xb'
+
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	#if not os.getenv("SECRET_KEY"):
+	#	raise RuntimeError("SECRET_KEY is not set")
+
+	
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #    'sqlite:///' + os.path.join(basedir, 'app.db')
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
