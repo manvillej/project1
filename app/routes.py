92b1f7e35362f1f6989e8bcbc6e08335b811ab1c
manvillej <jeffmanvillejr@gmail.com>
2018-07-08 01:13:16 -0400

    added all necessary routes
    next adding base template

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,33 @@ from app import app
 def index():
     user = {'username':'Jeff'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login')
+def login():
+    user = {'username':'Jeff'}
+    return render_template('index.html', title='login', user=user)
+
+@app.route('/logout')
+def logout():
+    user = {'username':'Jeff'}
+    return render_template('index.html', title='logout', user=user)
+
+@app.route('/register')
+def register():
+    user = {'username':'Jeff'}
+    return render_template('index.html', title='register', user=user)
+
+@app.route('/search')
+def search():
+    user = {'username':'Jeff'}
+    return render_template('index.html', title='search', user=user)
+s
+@app.route('/location/<int:zipcode>')
+def location(zipcode):
+    user = {'username':'Jeff'}
+    return render_template('index.html', title='location', user=user)
+
+@app.route('/api/<int:zipcode>')
+def api(zipcode):
+    user = {'username':'Jeff'}
+    return render_template('index.html', title='api', user=user)
