02410cf4a6ed819e40858f8fcc8a83fda20883ae
manvillej <jeffmanvillejr@gmail.com>
2018-07-10 19:57:46 -0400

    login redirect, login required, formatting
    added login required to some of the pages. I may change the index to be a landing page for anonymous users and registered users
    
    I also:
     - added a login redirect
     - reformatted render template lines
    
    I also

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, site_config
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route("/")
+@login_required
 def index():
-    return render_template('index.html', title='Home', config=site_config)
+    return render_template(
+        'index.html',
+        title='Home',
+        config=site_config)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -25,7 +30,12 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(url_for(next_page))
 
     return render_template(
         'login.html', 
@@ -44,23 +54,41 @@ def logout():
 @app.route('/register')
 def register():
     user = {'username':'Jeff'}
-    return render_template('index.html', title='register', config=site_config, user=user)
+    return render_template(
+        'index.html',
+        title='register',
+        config=site_config,
+        user=user)
 
 
 @app.route('/search')
+@login_required
 def search():
     user = {'username':'Jeff'}
-    return render_template('index.html', title='search', config=site_config, user=user)
+    return render_template(
+        'index.html',
+        title='search',
+        config=site_config,
+        user=user)
 
 
 @app.route('/location/<int:zipcode>')
+@login_required
 def location(zipcode):
     user = {'username':'Jeff'}
-    return render_template('index.html', title='location', config=site_config, user=user)
+    return render_template(
+        'index.html',
+        title='location',
+        config=site_config,
+        user=user)
 
 
 @app.route('/api/<int:zipcode>')
 def api(zipcode):
     user = {'username':'Jeff'}
-    return render_template('index.html', title='api', config=site_config, user=user)
+    return render_template(
+        'index.html',
+        title='api',
+        config=site_config,
+        user=user)
 
