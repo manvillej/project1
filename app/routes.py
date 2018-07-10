26ba688b54a46018178560eb15da75ff473355be
manvillej <jeffmanvillejr@gmail.com>
2018-07-10 19:35:57 -0400

    Added login/logout support
    added login/logout functionality and added a bit to jinjia to show that it works.

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,44 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, site_config
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 
 @app.route("/")
 def index():
-    user = {'username':'Jeff'}
-    return render_template('index.html', title='Home', config=site_config, user=user)
+    return render_template('index.html', title='Home', config=site_config)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(f'login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    user = {'username':'Jeff'}
-    return render_template('login.html', title='login', config=site_config, form=form, user=user)
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
+    return render_template(
+        'login.html', 
+        title='login', 
+        config=site_config, 
+        form=form)
 
 
 @app.route('/logout')
 def logout():
-    user = {'username':'Jeff'}
-    return render_template('index.html', title='logout', config=site_config, user=user)
+    logout_user()
+
+    return redirect(url_for('index'))
 
 
 @app.route('/register')
