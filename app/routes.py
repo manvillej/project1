46633aa05ca0899f58aaa97918476d27754456b3
manvillej <jeffmanvillejr@gmail.com>
2018-07-10 20:34:32 -0400

    registration, register link, next page
    added registration form,
    added register link to login page,
    added next page feature to redirect user back to last page.

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app, site_config
-from app.forms import LoginForm
+from app import app, db, site_config
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 
 
 @app.route("/")
+@app.route("/index")
 @login_required
 def index():
     return render_template(
@@ -34,8 +35,8 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-
-        return redirect(url_for(next_page))
+            
+        return redirect(next_page)
 
     return render_template(
         'login.html', 
@@ -47,18 +48,28 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-
     return redirect(url_for('index'))
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    user = {'username':'Jeff'}
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = RegistrationForm()
+ 
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+
     return render_template(
-        'index.html',
+        'register.html',
         title='register',
-        config=site_config,
-        user=user)
+        config=site_config, form=form)
 
 
 @app.route('/search')
