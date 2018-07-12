4ac6a077932b2a04eb3b61966e737d413406a2e0
manvillej <jeffmanvillejr@gmail.com>
2018-07-12 02:26:49 -0400

    Added CheckIn model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from datetime import datetime
 
 
 class User(UserMixin, db.Model):
@@ -9,6 +10,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    checkins = db.relationship("User", backref="CheckIn", lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -32,7 +34,17 @@ class Location(db.Model):
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     population = db.Column(db.Integer)
+    checkins = db.relationship("Location", backref="CheckIn", lazy=True)
 
     def __repr__(self):
         return f'<Location - ({self.longitude}, {self.latitude}), ({self.city}, {self.state} {self.zipcode})>'.format()
 
+class CheckIn(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    body = db.Column(db.String(120))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+
+    def __repr__(self):
+        return f'<CheckIn {self.user_id}, {self.location_id}>'
